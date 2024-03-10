38cf379f0d56b5965d80136c27f7948021cf464b
Hriday Desai <126891488+Dogtags27@users.noreply.github.com>
2024-03-11 03:45:52 +0530

    Update record.py

--- record.py
+++ record.py
@@ -3,12 +3,17 @@ import time
 
 # Set PYTHONPATH environment variable
 subprocess.run('set PYTHONPATH=.', shell=True)
-
+time.sleep(3)
 # Run 'python main.py cam' in a separate process
-subprocess.Popen(['python', 'main.py', 'cam'])
+cam_process=subprocess.Popen(['python', 'main.py', 'cam'])
 
 # Wait for 1 second
-time.sleep(10)
+# time.sleep(1)
 
 # Run 'python main.py mic' in the current process
-subprocess.run(['python', 'main.py', 'mic'])
+audio_process=subprocess.run(['python', 'main.py', 'mic'])
+
+# time.sleep(1)
+# cam_process.terminate()
+# audio_process.terminate()
+
